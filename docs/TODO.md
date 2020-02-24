c4ce1156478a0077d9bd4e1f1492ffb70ee9c211
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-02-24 09:33:29 +0200

    Deleting webapp specific code: Deleted src files

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,3 +1,4 @@
 * [x] Run app and safe remove brain dir
 * [x] Update readme from old template
 * [x] Merge with latest app
+* [ ] Change ReadMe
--- src/main/node/electron/run/babel.js
+++
@@ -1,3 +0,0 @@
-const {register} = require('../../../../../env/babel/helpers')
-const babelrc = require('../../../../../env/babel/configs/mocha')
-register(babelrc)
