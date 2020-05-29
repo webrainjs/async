2ee146b2e1dccaef38cc2d01622534782dd66869
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-05-29 07:49:51 +0300

    Deleted unnecessary files

--- src/initWebrain.js
+++
@@ -1,7 +0,0 @@
-import {
-	webrainOptions,
-} from 'webrain'
-// noinspection NpmUsedModulesInstalled
-import appConfig from 'APP_CONFIG_PATH'
-
-webrainOptions.callState.logCaughtErrors = !!appConfig.dev
--- src/main/node/electron/run/dev-vm/app.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable:no-var-requires */
-import {init} from '../init'
-const { app } = require('electron')
-import appConfig from '../../../../../../configs/debug'
-
-init(app, appConfig, () => `http://192.168.0.102:${appConfig.sapper.port}/app/`)
