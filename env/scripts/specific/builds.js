8a362acee339f7a0734ff8af6c544b8ab7ca164d
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-05-31 07:40:26 +0300

    Fix scripts: clean before build

--- env/scripts/specific/builds.js
+++ env/scripts/specific/builds.js
@@ -3,38 +3,46 @@ const common = require('../common')
 const {reCreateDir, deletePaths} = require('../../common/helpers')
 
 const buildMjs = singleCall(async appConfigType => {
-	await reCreateDir(`dist/${appConfigType}/mjs/`)
+	await reCreateDir(`dist/${appConfigType}/mjs`)
 	await run(`cpy "**/*.(css|html|svelte)" "../dist/${appConfigType}/mjs/" --parents --cwd=src`)
 	await run(`babel src -x .js -x .ts --out-dir dist/${appConfigType}/mjs/ --no-babelrc --config-file ./env/babel/configs/dist-mjs.js`)
 })
 const buildJs = singleCall(async appConfigType => {
-	await reCreateDir(`dist/${appConfigType}/js/`)
+	await reCreateDir(`dist/${appConfigType}/js`)
 	await run(`cpy "**/*.(css|html|svelte)" "../dist/${appConfigType}/js/" --parents --cwd=src`)
 	await run(`babel src -x .js -x .ts --out-dir dist/${appConfigType}/js/ --no-babelrc --config-file ./env/babel/configs/dist-js.js`)
 })
 const buildComponents = singleCall(async appConfigType => {
-	await deletePaths(`dist/${appConfigType}/components/`)
+	await deletePaths(`dist/${appConfigType}/components`)
 	await run(
 		'rollup --config ./env/rollup/components.js',
 		{env: {APP_CONFIG: appConfigType}}
 	)
 })
 
-const buildSapperExport = singleCall(appConfigType => run(
-	`sapper export "dist/${appConfigType}/sapper/export/"`
-	+ ` --build-dir tmp/${appConfigType}/sapper/build`
-	+ ` --output src/node_modules/@sapper/${appConfigType}`
-	+ ' --timeout 180000 --legacy --basepath app',
-	{
-		env: {
-			APP_CONFIG: appConfigType,
-			PORT      : require(`../../../configs/${appConfigType}`).sapper.port,
+const buildSapperExport = singleCall(async appConfigType => {
+	await deletePaths(
+		`src/node_modules/@sapper/${appConfigType}`,
+		`tmp/${appConfigType}/sapper/export`,
+		`dist/${appConfigType}/sapper/build`,
+	)
+
+	await run(
+		`sapper export "dist/${appConfigType}/sapper/export/"`
+		+ ` --build-dir tmp/${appConfigType}/sapper/build`
+		+ ` --output src/node_modules/@sapper/${appConfigType}`
+		+ ' --timeout 180000 --legacy --basepath app',
+		{
+			env: {
+				APP_CONFIG: appConfigType,
+				PORT: require(`../../../configs/${appConfigType}`).sapper.port,
+			},
 		},
-	},
-))
+	)
+})
 
 const buildElectron = singleCall(async appConfigType => {
-	await deletePaths(`dist/${appConfigType}/electron/build/`)
+	await deletePaths(`dist/${appConfigType}/electron/build`)
 	await run(
 		'node env/electron/build.js',
 		{env: {APP_CONFIG: appConfigType}}
@@ -43,14 +51,14 @@ const buildElectron = singleCall(async appConfigType => {
 })
 
 const buildChrome = singleCall(async appConfigType => {
-	await deletePaths(`dist/${appConfigType}/chrome/build/`)
+	await deletePaths(`dist/${appConfigType}/chrome/build`)
 	await run(
 		'node env/chrome/build.js',
 		{env: {APP_CONFIG: appConfigType}}
 	)
 })
 
-const clean = singleCall(appConfigType => deletePaths(`{dist,tmp}/${appConfigType}`))
+const clean = singleCall(appConfigType => deletePaths(`{dist,tmp,src/node_modules/@sapper}/${appConfigType}`))
 const build = singleCall(async appConfigType => {
 	// await clean(appConfigType)
 	// await run('echo      │ node_modules\\core-js-pure\\stable\\set-interval.js (0.2%)\n')
